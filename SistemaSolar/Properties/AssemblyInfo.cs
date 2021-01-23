056ae11e76bbdd839c6127d49a8c4da5ad201057
Oğuzhan ÇART <54273323+oguzhan18@users.noreply.github.com>
2021-01-23 04:45:54 +0300

    Add files via upload

---
+++ SistemaSolar/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("SistemaSolar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SistemaSolar")]
+[assembly: AssemblyProduct("SistemaSolar")]
+[assembly: AssemblyCopyright("Copyright © Sistema 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c8169e9c-2e8a-4bd5-a9eb-ab1db5009b13")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
